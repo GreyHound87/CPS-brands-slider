5799ef2463cafc5d243a45cfd4776c01269f986e
Sergei Smirnov <sasmirnov87@gmail.com>
2023-07-10 01:05:44 +0300

    more-btn for brands

--- scripts/moreBtn.js
+++ scripts/moreBtn.js
@@ -1,12 +1,10 @@
 const brandsMoreBtn = document.getElementById('brandsMoreBtn');
-const brandsMoreBtnImg = document.getElementById('brandsMoreBtnImg');
 const brandsMoreBtnTxt = document.getElementById('brandsMoreBtnTxt');
 const brandsSwiperContainer = document.getElementById('brandsSwiperContainer');
 
 brandsMoreBtn.addEventListener('click', function() {
   brandsSwiperContainer.classList.toggle('brands__swiper-container--expanded');
   brandsMoreBtn.classList.toggle('brands__more-btn--active');
-  brandsMoreBtnImg.classList.toggle('more-btn__img--flip');
   brandsMoreBtnTxt.textContent = brandsMoreBtn.classList.contains('brands__more-btn--active') ? 'Скрыть' : 'Показать все';
 
   const brandsSwiperSlides = document.querySelectorAll('.brands__swiper-slide');
